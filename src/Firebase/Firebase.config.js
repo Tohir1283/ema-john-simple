9d40c8d94d37dec7675cc2aa77474fde27f09b3d
Tohir1283 <tohirul.islam164@gmail.com>
2021-10-13 09:00:29 +0600

    Firebase

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCpRSy0KldXrBCWUjErtDHVJeswG-be3KE",
+  authDomain: "ema-john-simple-c7d80.firebaseapp.com",
+  projectId: "ema-john-simple-c7d80",
+  storageBucket: "ema-john-simple-c7d80.appspot.com",
+  messagingSenderId: "693792004932",
+  appId: "1:693792004932:web:fc3e90ccc8127b6236d511",
+};
+
+export default firebaseConfig;
---
+++ src/Firebase/Firebase.init.js
@@ -0,0 +1,9 @@
+// @ts-nocheck
+import firebaseConfig from "./Firebase.config";
+import { initializeApp } from "firebase/app";
+
+const initializeAuthentication = () => {
+  initializeApp(firebaseConfig);
+};
+
+export default initializeAuthentication;
